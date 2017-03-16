7b544fec34f9a8fe47135468f639433d39122521
jakearthur22 <harmonicadude22@gmail.com>
2017-03-16 15:08:14 -0400

    Update CardGUI.java

--- pharm-cards/CardGUI.java
+++ pharm-cards/CardGUI.java
@@ -7,7 +7,7 @@ import javax.util.*;
  *
  *
  * 
- * @author Jake
+ * @author Jake Arthur
  * @version 2017.13.3
  */
 public class CardGUI extends JFrame
@@ -15,10 +15,31 @@ public class CardGUI extends JFrame
   private Card card;
   private ArrayList<Element> els;
   private ArrayList<JButton> jbutts;
+  private JTextArea outputTxt;
   
   public CardGUI(Card c)
   {
     card = c;
+    els = card.getEls();
+    jbutts = new ArrayList<JButton>;
+    initComponents();
+    this.setVisible(true);
+  }
+  
+  private void addElButtons()
+  {
+    int i = 1;
+    for(Element el : els)
+    {
+      jbutts.add(new JButton(el.getName()));
+      i++;
+    }
+  }
+  
+  private void initComponents()
+  {
+    addElButtons();
+    outputTxt = new JTextArea();
     
   }
   
