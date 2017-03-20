308c3f64f88bd3eeaeadbedd0b2b4fa4f1f44ed7
jakearthur22 <harmonicadude22@gmail.com>
2017-03-20 10:14:59 -0400

    Update CardGUI.java

--- pharm-cards/CardGUI.java
+++ pharm-cards/CardGUI.java
@@ -16,12 +16,15 @@ public class CardGUI extends JFrame
   private ArrayList<Element> els;
   private ArrayList<JButton> jbutts;
   private JTextArea outputTxt;
+  private JScrollPane pane;
+  private boolean pressed;
   
   public CardGUI(Card c)
   {
     card = c;
     els = card.getEls();
     jbutts = new ArrayList<JButton>;
+    pressed = false;
     initComponents();
     this.setVisible(true);
   }
@@ -34,14 +37,57 @@ public class CardGUI extends JFrame
       jbutts.add(new JButton(el.getName()));
       i++;
     }
-  }
+  } //end of addElButtons()
   
   private void initComponents()
   {
     addElButtons();
     outputTxt = new JTextArea();
+    pane = new JScrollPane()
     
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-  }
+    
+    outputTxt.setColumns(20);
+    outputTxt.setRows(5);
+    pane.setViewportView(outputTxt);
+    
+    //for loop to set the name of each element
+    int i = 0;
+    for(Element el : els)
+    {
+      jbutts.get(i).setText(el.getName());
+      i++;
+    }
+    
+    //for loop to set the output text for each element
+    i = 0;
+    for (Element el : els)
+    {
+      jbutts.get(i).addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent evt)
+        {
+          if(pressed = false)
+          {
+            outputTxt.setText(el.getText());
+            pressed = true;
+          }
+          else
+          {
+            outputTxt.setText("");
+            pressed = false;
+          }
+        }
+      });
+      i++;
+    }// end of for loop to set the output text for each element
+    
+    FlowLayout layout = new FlowLayout(getContentPane());
+    getContentPane.setLayout(layout);
+    //put the code for the layout here. Refer to notes.
+    
+  }// end of initComponents()
   
-}
+  
+  
+}//end of class
