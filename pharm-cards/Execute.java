28df9a995f9daa7cbd8266a11c626416bb6a268a
jakearthur22 <harmonicadude22@gmail.com>
2017-03-21 10:22:36 -0400

    Update Execute.java

--- pharm-cards/Execute.java
+++ pharm-cards/Execute.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+
+public class Execute()
+{
+  private Scanner scanner;
+  
+  
+  public Execute()
+  {
+    scanner = new Scanner([insert filname here]);  //insert filename here
+    init()
+  }
+  
+  private void init()
+  {
+    
+  }
+  
+}
