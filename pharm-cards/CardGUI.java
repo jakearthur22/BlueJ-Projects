6f4be884b81e92919e40f07622057c2036194a5e
jakearthur22 <harmonicadude22@gmail.com>
2017-03-21 09:59:07 -0400

    Update CardGUI.java

--- pharm-cards/CardGUI.java
+++ pharm-cards/CardGUI.java
@@ -85,9 +85,39 @@ public class CardGUI extends JFrame
     FlowLayout layout = new FlowLayout(getContentPane());
     getContentPane.setLayout(layout);
     //put the code for the layout here. Refer to notes.
+    layout.setAlignment(FlowLayout.CENTER);
     
   }// end of initComponents()
   
-  //put main(String args[]) here
+  public static void main(String args[])
+  {
+    /* Set the Nimbus look and feel */
+    try
+    {
+      for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+      {
+        if ("Nimbus".equals(info.getName()))
+        {
+          javax.swing.UIManager.setLookAndFeel(info.getClassName());
+          break;
+        }
+      }
+    }
+    catch (ClassNotFoundException ex)
+    {
+      java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    catch (InstantiationException ex)
+    {
+      java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    catch (IllegalAccessException ex)
+    {
+      java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    catch (javax.swing.UnsupportedLookAndFeelException ex)
+    {
+      java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
   
 }//end of class
