6e3087122db1e79e5013ed60a177fc14e432c1d4
jakearthur22 <harmonicadude22@gmail.com>
2017-03-22 16:29:07 -0400

    Update MainGUI.java

--- pharm-cards/MainGUI.java
+++ pharm-cards/MainGUI.java
@@ -1,10 +1,12 @@
 public class MainGUI() extends JFrame
 {
   private Librarian librarian;
+  private ArrayList<Card> library;
   
   public MainGUI()
   {
     librarian = new Librarian(new File(/*file location*/);
+    library = librarian.getLibrary();
   }
   
 }
