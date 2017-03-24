7a43c6bc060bead12d33913d27989d2739363d5c
jakearthur22 <harmonicadude22@gmail.com>
2017-03-24 18:07:32 -0400

    Update MainGUI.java

--- pharm-cards/MainGUI.java
+++ pharm-cards/MainGUI.java
@@ -41,7 +41,7 @@ public class MainGUI extends JFrame
   {
     for(Card card : library)
     {
-      if(card.getName().equals(cat)))
+      if(card.getCategory().equals(cat)))
       {
         drugList[drugList.length] = card.getName();
       }
@@ -51,11 +51,22 @@ public class MainGUI extends JFrame
   public void addComponentsToPane(final Container pane)
   {
     //create JPanels and set layout manager
-    JPanel pickCardPanel = new JPanel(new CardLayout());
+    JPanel pickCardPanel = new JPanel();
+    pickCardPanel.setLayout(new BoxLayout(pickCardPanel, BoxLayout.Y_AXIS));
       JPanel pickPanel1 = new JPanel();
       JPanel pickPanel2 = new JPanel();
       JPanel pickPanel3 = new JPanel();
       JPanel pickPanel4 = new JPanel();
+    
+    JPanel addCardPanel = new JPanel();
+    addCardPanel.setLayout(new BoxLayout(addCardPanel, BoxLayout.Y_AXIS));
+      JPanel addPanel1 = new JPanel();
+      JPanel addPanel2 = new JPanel();
+      JPanel addPanel3 = new JPanel();
+      JPanel addPanel4 = new JPanel();
+      JPanel addPanel5 = new JPanel();
+      JPanel addPanel6 = new JPanel();
+      JPanel addPanel7 = new JPanel();
     //create components
     JLabel titleLabel = new JLabel("Pick a card:");
     JLabel catLabel = new JLabel("Category: ");
@@ -64,10 +75,58 @@ public class MainGUI extends JFrame
     JComboBox drugBox = new JComboBox(drugList);
     JButton selectDrugButton = new JButton("Select");
     
+    JLabel addDrugNameLabel = new JLabel("Drug Name: );
+    JTextField addDrugNameText = new JTextField(15);
+    JLabel addCatLabel = new JLabel("Category: ");
+    JTextField addCatText = new JTextField(15);
+    JLabel addExtraTextLabel = new JLabel("Main card text(optional): ");
+    JTextField addExtraTextText = new JTextField(15,15);
+    JLabel el1NameLabel = new JLabel("Element Name: ");
+    JTextField el1NameText = new JTextField(15);
+    JLabel el1TextLabel = new JLabel("Element Text: ");
+    JTextField el1TextText = new JTextField(15,15);
+    JButton addMoreElsButton = new JButton("Add Another Element");
+    JButton addCardButton = new JButton("Done: Add Card");
+    
     //add components to secondary panes
-    pickPanel1.add
-    //create action listeners for buttons
+    pickPanel1.add(titleLabel);
+    pickPanel2.add(catLabel); pickPanel2.add(catBox);
+    pickPanel3.add(drugLabel); pickPanel3.add(drugBox);
+    pickPanel4.add(selectDrugButton);
+    
+    //create action listeners for components
+    catBox.addActionListener(new ActionListener()
+    {
+      public void catBoxActionPerformed(ActionEvent evt)
+      {
+        setDrugList(catBox.getSelectedItem());
+        drugBox.removeAllItems();
+        for(int i = 0; i < drugList.length; i++)
+        {
+          drugBox.addItem(drugList[i]);
+        }
+      }
+    });
+    
+    selectDrugButton.addActionListener(new ActionListener()
+    {
+      public void selectDrugButtonActionPerformed(ActionEvent evt)
+      {
+        if(drugBox.getSelectedItem() != null)
+        {
+          for(Card card : library)
+          {
+            if(card.getName().equals(drugBox.getSelectedItem().toString()))
+            {
+              CardGUI cardDisplay = new CardGUI(card);
+              cardDisplay.setVisible(true);
+            }
+          }
+        }
+      }
+    }
     //add all to the main pane
+    pane.add(pickCardPanel, BorderLayout.LINE_START);
   }
   
   private static void createAndShowGUI()
